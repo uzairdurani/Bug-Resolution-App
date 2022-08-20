6ff834346762231c9dd9dd0179994adc0f3fe799
Uzair Durani <82148310+uzairdurani@users.noreply.github.com>
2022-08-20 11:26:50 -0700

    Delete myEnvVal.py

--- CommuteApp/myEnvVal.py
+++
@@ -1,9 +0,0 @@
-import os
-# Set environment variables
-
-
-def setVar():
-    os.environ['EMAIL_USER'] = 'muhammeduzair098@gmail.com'
-    os.environ['EMAIL_PASSWORD'] = 'UzairGmail14137#@!khan'
-# UzairGmail14137#@!khan
-# ioig naxw seaz dakz
